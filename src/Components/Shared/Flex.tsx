028d1f2e157a38e1ab9af6ed54c5ec3e93e165d8
Sabbaghh <your_email@example.com>
2023-03-07 17:02:41 +0400

    Creating Flex Component

---
+++ src/Components/Shared/Flex.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+interface ContainerProps {
+	ratio?: number;
+	justify?: string;
+	align?: string;
+	direction?: string;
+}
+export default styled.div<ContainerProps>`
+	display: flex;
+	flex: ${(props) => props.ratio || 1};
+	padding: ${(props) => props.theme.padding.xsm};
+	justify-content: ${(props) => props.justify || 'center'};
+	align-items: ${(props) => props.align || 'center'};
+	flex-direction: ${(props) => props.direction || 'column'};
+	height: 100%;
+	@media (min-width: 850px) {
+		height: 70%;
+	}
+`;
