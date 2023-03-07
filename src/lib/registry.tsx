10a776553136862107c1ae9955f228fdd2ceb1c2
Sabbaghh <your_email@example.com>
2023-03-07 17:13:59 +0400

    Removing Property 'clearTag' from registery file

--- src/lib/registry.tsx
+++ src/lib/registry.tsx
@@ -15,7 +15,6 @@ export default function StyledComponentsRegistry({
 
 	useServerInsertedHTML(() => {
 		const styles = styledComponentsStyleSheet.getStyleElement();
-		styledComponentsStyleSheet.instance.clearTag();
 		return <>{styles}</>;
 	});
 
