08393e4a6a09d76ef9eb17b345f80d57a5117eab
Sabbaghh <your_email@example.com>
2023-03-07 16:59:22 +0400

    Creating Styled-Components theme

---
+++ src/themes/Primary.ts
@@ -0,0 +1,27 @@
+const colors = {
+	primary: '#2C395F',
+	secondary: '#FF00F5',
+	primaryLight: '#8892B0',
+	background: '#FFFFFF',
+};
+
+export default {
+	colors: {
+		...colors,
+	},
+	fontSize: {
+		xxlg: '80px',
+		xlg: '64px',
+		lg: '32px',
+		md: '20px',
+		sm: '15px',
+		xsm: '12px',
+		xxsm: '10px',
+	},
+	padding: {
+		lg: '0 150px',
+		md: '0 100px',
+		sm: '0 50px',
+		xsm: '0 10px',
+	},
+};
