1bb1a0335bc68855a7c71dc6eccbfc2ebd745fe6
Sabbaghh <your_email@example.com>
2023-04-10 14:05:16 +0400

    Create reuseable ImageContainer component

--- src/Components/Shared/ImageContainer.tsx
+++ src/Components/Shared/ImageContainer.tsx
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 
 const Image = 'https://via.placeholder.com/500x500';
 interface AvatarContainerProps {
-	uri: string;
+	uri: string | undefined;
 }
 export default styled.div`
 	width: 100%;
 	height: 100%;
 	background-size: cover;
 	background-position: center;
+	background-position: bottom;
 	background-image: url(${(props: AvatarContainerProps) => props.uri || Image});
 `;
