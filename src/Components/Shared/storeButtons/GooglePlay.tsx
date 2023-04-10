1174c2267d078e7e5dd2931a0ddc2abe0c5575c0
Sabbaghh <your_email@example.com>
2023-04-10 14:06:55 +0400

    Create Appstore & playstore button

---
+++ src/Components/Shared/storeButtons/GooglePlay.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+const Icon = styled.svg`
+	width: 90%;
+`;
+function GooglePlay() {
+	return (
+		<Icon viewBox='0 0 25 26' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<g clipPath='url(#clip0_63_2453)'>
+				<path
+					d='M15.8838 11.8057L5.10742 1L18.8184 8.87109L15.8838 11.8057ZM2.29492 0.365234C1.66016 0.697266 1.23535 1.30273 1.23535 2.08887V23.6367C1.23535 24.4229 1.66016 25.0283 2.29492 25.3604L14.8242 12.8604L2.29492 0.365234ZM23.0566 11.3809L20.1807 9.71582L16.9727 12.8652L20.1807 16.0146L23.1152 14.3496C23.9941 13.6514 23.9941 12.0791 23.0566 11.3809ZM5.10742 24.7305L18.8184 16.8594L15.8838 13.9248L5.10742 24.7305Z'
+					fill='#2C395F'
+				/>
+			</g>
+			<defs>
+				<clipPath id='clip0_63_2453'>
+					<rect
+						width='25'
+						height='25'
+						fill='white'
+						transform='translate(0 0.365234)'
+					/>
+				</clipPath>
+			</defs>
+		</Icon>
+	);
+}
+
+export default GooglePlay;
