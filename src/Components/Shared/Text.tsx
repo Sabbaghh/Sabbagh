87fafadee0d9f3cf41adbc36d312ba368a687197
Sabbaghh <your_email@example.com>
2023-04-10 14:05:40 +0400

    Create reuseable text component

--- src/Components/Shared/Text.tsx
+++ src/Components/Shared/Text.tsx
@@ -1,71 +1,84 @@
 import React from 'react';
 import localFont from 'next/font/local';
 import styled from 'styled-components';
+import { Cairo } from 'next/font/google';
 const SFMono = localFont({ src: '../../Fonts/SFMonoRegular.otf' });
 const CalibreRegular = localFont({ src: '../../Fonts/CalibreRegular.otf' });
 const CalibreSemibold = localFont({ src: '../../Fonts/CalibreSemibold.otf' });
+const cairo = Cairo({
+	weight: '400',
+	style: 'normal',
+	subsets: ['latin'],
+});
+const cairoSemiBold = Cairo({
+	weight: '700',
+	style: 'normal',
+	subsets: ['latin'],
+});
 
 interface Props {
 	type?: string;
 	size?: string;
 	color?: string;
-	style?: string;
-	text?: string;
-	mobileSize?: boolean;
+	text: string;
+	align?: string;
 }
-function Text({
-	size = 'sm',
-	color = 'primary', //primary, secondary, primaryLight , background
-	style = 'regular', //bold
-	type = 'primary', //secondary
-	text,
-}: Props) {
-	if (type === 'secondary') {
-		return (
-			<StyledText
-				type={type}
-				size={size}
-				className={SFMono.className}
-				color={color}
-			>
-				{text}
-			</StyledText>
-		);
-	} else {
-		if (style === 'regular') {
+
+export const types = {
+	primaryBold: 'primaryBold',
+	primaryRegular: 'primaryRegular',
+	secondaryRegular: 'secondaryRegular',
+};
+function Text(Props: Props) {
+	switch (Props.type) {
+		case types.primaryBold:
 			return (
-				<StyledText
-					type={type}
-					size={size}
-					className={CalibreRegular.className}
-					color={color}
-				>
-					{text}
+				<StyledText {...Props} className={cairoSemiBold.className}>
+					{Props.text}
 				</StyledText>
 			);
-		} else {
+		case types.primaryRegular:
 			return (
-				<StyledText
-					type={type}
-					size={size}
-					className={CalibreSemibold.className}
-					color={color}
-				>
-					{text}
+				<StyledText {...Props} className={cairo.className}>
+					{Props.text}
 				</StyledText>
 			);
-		}
+		case types.secondaryRegular:
+			return (
+				<StyledText {...Props} className={SFMono.className}>
+					{Props.text}
+				</StyledText>
+			);
+		default:
+			return (
+				<StyledText {...Props} className={cairo.className}>
+					{Props.text}
+				</StyledText>
+			);
 	}
 }
 
 const StyledText = styled.span<Props>`
+	text-align: ${(props) => (props.align ? props.align : 'left')};
 	color: ${(props) =>
-		props.color
-			? props.theme.colors[props.color]
-			: props.theme.colors.secondary};
+		props.color ? props.theme.colors[props.color] : props.theme.colors.primary};
 	font-size: ${(props) =>
-		props.size ? props.theme.fontSize[props.size] : props.theme.fontSize.md};
-
-	/* border: 1px solid red; */
+		props.size
+			? `${props.theme.fontSizeSmallScreen[props.size]}rem`
+			: `${props.theme.fontSizeSmallScreen.md}rem`};
+	line-height: ${(props) =>
+		props.size
+			? `${props.theme.fontSizeSmallScreen[props.size]}rem`
+			: `${props.theme.fontSizeSmallScreen.md}rem`};
+	@media (min-width: 1400px) {
+		font-size: ${(props) =>
+			props.size
+				? `${props.theme.fontSize[props.size]}rem`
+				: `${props.theme.fontSize.md}rem`};
+		line-height: ${(props) =>
+			props.size
+				? `${props.theme.fontSize[props.size]}rem`
+				: `${props.theme.fontSize.md}rem`};
+	}
 `;
 export default Text;
