fdbc8bdecb566865d65cf10bffeb7caca7fb3fc9
Sabbaghh <your_email@example.com>
2023-04-10 14:06:08 +0400

    Create reuseable Section component

---
+++ src/Components/Shared/Section.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+export default styled.section`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	overflow: hidden;
+`;
