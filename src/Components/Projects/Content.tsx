072bae748c856befa77d50f855fd0924eac43d1c
Sabbaghh <your_email@example.com>
2023-04-10 14:03:34 +0400

    Wrapping up Projects Section

---
+++ src/Components/Projects/Content.tsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import styled from 'styled-components';
+import Text, { types } from '../Shared/Text';
+import ProjectMeta from '../Shared/ProjectMeta';
+interface IProps {
+	tag: string;
+	title: string;
+	description: string;
+	subtitle: string;
+	tech: string[];
+	meta: {
+		downloads: string;
+		appstore: string;
+		playstore: string;
+	};
+}
+function Content(props: IProps) {
+	const { tag, title, description, subtitle, tech, meta } = props;
+	return (
+		<ContentContainer>
+			<ContentHeader>
+				<Text
+					size='xxxlg'
+					type={types.primaryBold}
+					color='primaryLight'
+					text={tag}
+				/>
+			</ContentHeader>
+			<ContentBody>
+				<Text
+					text={subtitle}
+					size='sm'
+					type={types.secondaryRegular}
+					color='secondary'
+				/>
+				<Text text={title} size='xxlg' type={types.primaryBold} />
+				<Text
+					color='primaryLight'
+					text={description}
+					size='sm'
+					type={types.primaryRegular}
+				/>
+				<TechContainer>
+					{tech.map((tech, i) => {
+						return (
+							<Tech key={i}>
+								<Text text={tech} size='xsm' type={types.secondaryRegular} />
+							</Tech>
+						);
+					})}
+				</TechContainer>
+			</ContentBody>
+			<ContentFooter>
+				<FooterItem>
+					<ProjectMeta meta={meta} />
+				</FooterItem>
+			</ContentFooter>
+		</ContentContainer>
+	);
+}
+
+const ContentContainer = styled.div`
+	width: min(900px, 85%);
+	height: max(600px, 85%);
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	flex-direction: column;
+`;
+
+const ContentHeader = styled.div`
+	opacity: 0.8;
+	flex: 3;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: flex-start;
+	flex-direction: column;
+`;
+
+const ContentBody = styled.div`
+	flex: 2;
+	width: 100%;
+	display: flex;
+	justify-content: space-evenly;
+	align-items: flex-start;
+	flex-direction: column;
+`;
+
+const ContentFooter = styled.div`
+	flex: 1;
+	display: flex;
+	width: 100%;
+	justify-content: center;
+	align-items: flex-start;
+	flex-direction: column;
+`;
+
+const FooterItem = styled.div`
+	display: flex;
+	flex: 1;
+	flex-direction: row;
+	width: 100%;
+`;
+const TechContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+`;
+const Tech = styled.div`
+	display: block;
+	padding-right: 2rem;
+`;
+
+export default Content;
---
+++ src/Components/Projects/Projects.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import styled from 'styled-components';
+import Section from '../Shared/Section';
+import Card from '../Shared/Card';
+import { cardTypes } from '../Shared/Card/types';
+import Content from './Content';
+import data from '../../content/site-data.json';
+function Projects() {
+	return (
+		<>
+			{data.projects.map((project, i) => {
+				const {
+					title,
+					description,
+					avatar,
+					social,
+					tag,
+					subtitle,
+					tech,
+					meta,
+				} = project;
+				return (
+					<Section key={i}>
+						{i % 2 === 0 ? (
+							<>
+								<Left>
+									<Card
+										title={title}
+										description={description}
+										social={social}
+										meta={meta}
+										avatar={avatar}
+										type={cardTypes.project}
+									/>
+								</Left>
+								<Right hidesm={true}>
+									<Content
+										subtitle={subtitle}
+										tag={tag}
+										title={title}
+										description={description}
+										tech={tech}
+										meta={meta}
+									/>
+								</Right>
+							</>
+						) : (
+							<>
+								<Left hidesm={true}>
+									<Content
+										subtitle={subtitle}
+										tag={tag}
+										title={title}
+										description={description}
+										tech={tech}
+										meta={meta}
+									/>
+								</Left>
+
+								<Right>
+									<Card
+										title={title}
+										description={description}
+										social={social}
+										meta={meta}
+										avatar={avatar}
+										type={cardTypes.project}
+									/>
+								</Right>
+							</>
+						)}
+					</Section>
+				);
+			})}
+		</>
+	);
+}
+interface IProps {
+	hidesm?: boolean;
+}
+const Left = styled.div<IProps>`
+	display: ${(props) => (props.hidesm ? 'none' : 'flex')};
+	justify-content: ${(props) => (props.hidesm ? 'flex-end' : 'center')};
+	flex: 4;
+	align-items: center;
+	@media (min-width: 992px) {
+		display: flex;
+	}
+`;
+const Right = styled.div<IProps>`
+	display: ${(props) => (props.hidesm ? 'none' : 'flex')};
+	justify-content: ${(props) => (props.hidesm ? 'flex-start' : 'center')};
+	flex: 4;
+	@media (min-width: 992px) {
+		flex: 1;
+		display: flex;
+		align-items: center;
+		flex: 4;
+	}
+`;
+
+export default Projects;
