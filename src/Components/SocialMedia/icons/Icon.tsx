ad154d49d895468faf77899c6e9b2b9bce5a7d91
Sabbaghh <your_email@example.com>
2023-04-23 02:14:35 +0400

    Fixing social media icons on mobile

--- src/Components/SocialMedia/icons/Icon.tsx
+++ src/Components/SocialMedia/icons/Icon.tsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 const Icon = styled.svg`
+	width: 1.2em;
 	fill: ${(props) => props.theme.colors.primary};
 	:hover {
 		fill: ${(props) => props.theme.colors.secondary};
 	}
-
 	transition: fill 0.2s ease-in-out;
 	cursor: pointer;
 `;
