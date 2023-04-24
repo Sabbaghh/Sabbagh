35136bdd4d8ae476038b73d558fb975923ba9a15
Sabbaghh <your_email@example.com>
2023-04-24 14:59:23 +0400

    Adding site to the SEO

--- src/Components/Head.tsx
+++ src/Components/Head.tsx
@@ -4,7 +4,9 @@ import Header from 'next/head';
 const description = `
 Abdallah Sabbagh is a freelance frontend and mobile developer based in Dubai, UAE. With expertise in HTML, CSS, JavaScript, React, and React Native, he delivers high-quality results for clients. Hire Abdallah Sabbagh to elevate your web and mobile projects in Dubai and the UAE's dynamic tech scene.
 `;
-const img = '/img/avatar/avatar.jpg';
+
+const site = 'https://sabbagh.info';
+const img = `${site}/img/avatar/avatar.jpg`;
 function Head() {
 	return (
 		<Header>
@@ -36,12 +38,12 @@ function Head() {
 			<meta property='og:title' content='Abdallah Sabbagh' />
 			<meta property='og:description' content={description} />
 			<meta property='og:image' content={img} />
-			<meta property='og:url' content='https://sabbagh.info/' />
+			<meta property='og:url' content={site} />
 			<meta property='og:type' content='website' />
 
 			<meta name='twitter:card' content='summary_large_image' />
 			<meta name='twitter:site' content='@Sabbaghh_' />
-			<meta name='twitter:title' content='Your Website Title' />
+			<meta name='twitter:title' content='Abdallah Sabbagh' />
 			<meta name='twitter:description' content={description} />
 			<meta name='twitter:image' content={img}></meta>
 		</Header>
